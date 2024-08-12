a62f1e7bf9850ffec0e2a05a974ca6d4a1b6702a
Nikhil <nikhilpramod425@gmail.com>
2024-08-12 11:32:47 +0530

    completed

---
+++ script.js
@@ -0,0 +1,29 @@
+function toggleMenu() {
+    const menu = document.getElementById('navbarMenu');
+    menu.classList.toggle('active');
+    
+
+    const menuButton = document.querySelector('.menu-button');
+    const closeButton = document.querySelector('.close-button');
+    menuButton.style.display = menu.classList.contains('active') ? 'none' : 'block';
+    closeButton.style.display = menu.classList.contains('active') ? 'block' : 'none';
+}
+
+
+
+
+  
+new Glider(document.querySelector('.glider'), {
+    slidesToScroll: 1,
+    slidesToShow: 5.5,
+    draggable: true,
+    dots: '.dots',
+    arrows: {
+      prev: '.glider-prev',
+      next: '.glider-next'
+    }
+  });
+
+
+
+
